84f0620e4265445c61a35dad6366fbd5c89c2af1
johnugbor <ugborjohn@gmail.com>
2021-05-06 09:29:49 +0100

    updated reset password email confirmation

--- src/components/confirmresetpassword.js
+++ src/components/confirmresetpassword.js
@@ -2,14 +2,15 @@
 import React, {useState} from "react";
 import {Link, Redirect } from "react-router-dom";
 import axios from "axios";
-import Cookies from "js-cookie";
+
 import {rootUrl} from "../utilities/constants";
 import {confirmPasswordResetEndpoint} from "../utilities/endpoints"
 import {useParams} from "react-router-dom"; 
 
+import {Container,Image} from "react-bootstrap";
 import {useForm} from "react-hook-form";
 
-const csrfToken = Cookies.get('csrftoken');
+
 
 
 
@@ -21,46 +22,79 @@ export default function ConfirmResetPassword (props){
     
 
     const ResetPassword =(item)=>{
-            
-      axios.post(`${rootUrl}${confirmPasswordResetEndpoint}`,item,{headers:{'X-CSRFToken':csrfToken}}).then(
-       resp=>( setConfirmedReset(true))
-       )   
-       .catch(error=>error);
+         setIsError(false);
+         setIsLoading(true);   
+      axios.post(`${rootUrl}${confirmPasswordResetEndpoint}${uid}/${token}/`,item,).then(
+       (resp)=>{
+         if(resp.status === 200){
+          ( setConfirmedReset(true))
+          
+         }
+         else{setIsError(true);
+               setIsLoading(false);
+         
+         }
+          }) .catch(error=>{setIsError(true);
+            setIsLoading(false);});
+       
+        
+      
        
      };
      const {uid, token} = useParams();  
   const [isConfirmedReset, setConfirmedReset] = useState(false); 
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isConfirmed, setConfirmed] = useState(false);
 
   if(isConfirmedReset){
+    new Promise(resolve => { setTimeout(() => resolve(setConfirmed(true)), 3000); });
     return <Redirect push to = {{pathname:"/login"}} />;
   }
  else{
   return(
-
-    <form onSubmit={handleSubmit(ResetPassword)}>
-    <input type="hidden" name ="uid" value= {uid}/>
-    <input type="hidden" name ="token" value= {token}/>
+<Container className ="form-container">
+<h3 className="reset-password">Change Password</h3>
+ {isLoading&&<Image className="loadingspinner" src="/Iphone-spinner-2.gif"/>}
+ {isError&&<div><p>Error occured, retry</p></div>}
+ {isConfirmed&&<div><p>Paddword changed successful</p><Link to="/login" className="startdo-link">Login</Link></div>}
+    <form onSubmit={handleSubmit(ResetPassword)}
+     className="auth-form">
+    
 
     <div className ="form-row form-margin place-order-signup">
             <div className ="col">
-                <input type ="password" placeholder =" Create Password" 
+                <input type ="password" placeholder =" Enter new password" 
                     className ="form-control" name ="new_password1" 
                  
                     ref={register({
-                      required: "Password  is required",})}/>
-                     {errors.password && (
-                     <p style={{ color: "red" }}>{errors.password.message}</p>
+                      required: "Password  is required"
+                      ,minLength:8})}/>
+                     {errors.new_password1 && (
+                     <p style={{ color: "red" }}>{errors.new_password1.message}</p>
                     )}
+                    {errors.new_password1 && errors.new_password1.type === "minLength" && <span style={{ color: "red" }}>Enter atleast 8 characters</span>}
                     
             </div>
             
         </div>
+        <div className ="form-row form-margin place-order-signup">
+            <div className ="col"><input type="hidden"  name = "token"  
+            value = {token} className ="form-control" 
+            ref={register()}/></div></div>
+        <div className ="form-row form-margin place-order-signup">
+            <div className ="col"><input  type="hidden" name = "uid"  value = {uid} 
+            className ="form-control"  ref={register()}/></div></div>
+        
+            
+        
+
+                                     
+
     <div className ="form-row form-margin place-order-signup">
             <div className ="col">
                    <input type ="password" name ="new_password2" 
-                           placeholder ="Confirm Password" className ="form-control" 
+                           placeholder ="Confirm password" className ="form-control" 
                           
                             ref={register({
                             required: "Password confirmation is required",
@@ -70,12 +104,12 @@ export default function ConfirmResetPassword (props){
                            return new_password1 === value || 'Passwords should match!';
                     },},})}/>
                                 
-                {errors.confirmPassword && (
-                  <p style={{ color: "red" }}>{errors.confirmPassword.message}</p>
+                {errors.new_password2 && (
+                  <p style={{ color: "red" }}>{errors.new_password2.message}</p>
                  )}
             </div></div>
-    <input type="submit"  value ="Confirm Password Reset"/>
+    <input type="submit" disabled ={isLoading} value ="Confirm Password Reset" className="passwordresetbutton" />
   </form>
-
+  </Container>
 );
 }}
