8f778fdb91a0848704386bd35564086f0b09baae
johnugbor <ugborjohn@gmail.com>
2021-06-02 19:49:10 +0100

    removed redundant codes

--- src/App/store.js
+++
@@ -1,7 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import todayReducer from "../features/daydo/todayDoSlice"
-export const store = configureStore({
-    reducer:{
-        today : todayReducer,
-    },
-})
